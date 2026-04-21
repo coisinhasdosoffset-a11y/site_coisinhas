9bca691f0ea1eaa0ff81ec4be38ca9e5109e8f72
coisinhasdosoffset-a11y <coisinhasdosoffset@gmail.com>
2026-04-21 22:53:41 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,9 @@
 const { Pool } = require('pg');
 
+if (!process.env.DATABASE_URL) {
+  console.error('ERRO: DATABASE_URL não está definida.');
+}
+
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
